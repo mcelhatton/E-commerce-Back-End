22b6c5dfd6d42dd2b660374fe394aad471c77e83
mcelhatton <colinm@mobiledealerdata.com>
2022-05-29 12:53:38 -0500

    saving updates to models

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,13 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
-// tag model
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
